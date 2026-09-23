8093ba5a071f3dea2dece168847f9c1b98a122c5
kekekeke11 <529442056@qq.com>
2026-09-23 21:20:05 +0800

    day1-2

---
+++ day1-2/test.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+
+# client = OpenAI(
+#     api_key="ms-cb35df85-2cab-4c28-a8fc-bcbd1d84f1c3", # 请替换成您的ModelScope Access Token
+#     base_url="https://api-inference.modelscope.cn/v1/"
+# )
+# response = client.responses.create(
+#     model="Qwen/Qwen3.8-Flash-Next",
+#     input="hello"
+# )
+# print(response.model_dump_json())
+
+from openai import OpenAI
+
+from dotenv import load_dotenv
+
+load_dotenv()
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-3.5-turbo",
+    #tools=[{"type": "web_search"}],
+    input="What was a positive news story from today?",
+)
+
+print(response.output_text)
+
